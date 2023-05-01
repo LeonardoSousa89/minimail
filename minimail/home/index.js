2bc963ac6ba00675a5f23d8544fd05dc67bcd02c
leonardoSousa89 <leoprofessionallogytech@outlook.com>
2023-05-01 17:09:40 -0300

    '@v9'

--- minimail/home/index.js
+++ minimail/home/index.js
@@ -1,12 +1,54 @@
 const doc=document
 
 const obj={
+    sendbox: doc.getElementById('sendbox'),
+    inbox: doc.getElementById('inbox'),
     logout: doc.getElementById('logout'),
-    url_logout: ''
+    url_logout: 'http://127.0.0.1:34568/signout'
 }
+
+obj.sendbox.addEventListener('click', function(e){
+    e.preventDefault()
+
+    doc.location.href='../sendbox/index.html'
+})
+
+obj.inbox.addEventListener('click', function(e){
+    e.preventDefault()
+
+    doc.location.href='../inbox/index.html'
+})
 
 obj.logout.addEventListener('click', function(e){
     e.preventDefault()
 
-    doc.location.href='../index.html'
+    fetch(obj.url_logout, 
+        { method: 'POST', 
+          headers: { 
+            'Content-Type': 'application/json' 
+        }
+    }).then(response=>{
+
+        if(response.status === 201){
+
+            localStorage.removeItem('data')
+            doc.location.href='../index.html'
+        }
+        if(response.status !== 201){
+
+            alert("i'm sorry there's an error with server")
+        }
+
+    })
+
 })
+
+function verifyRoute(){
+    const data=localStorage.getItem('data')
+
+    if(!data){
+        doc.location.href='../index.html'
+    }
+}
+
+verifyRoute()
